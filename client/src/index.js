25983269dcb1d3495d64083e05b9500f6f47ad1d
xeusteerapat <xeus085@gmail.com>
2022-01-16 19:56:29 +0700

    :sparkles: [Update] setup client routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import Success from "./components/Success";
+import Cancel from "./components/Cancel";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='success' element={<Success />} />
+          <Route path='cancel' element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
