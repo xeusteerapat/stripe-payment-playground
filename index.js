b9ab1625f357bb477f1a67856d08ea3906c03e78
xeusteerapat <xeus085@gmail.com>
2022-01-16 19:37:17 +0700

    :sparkles: [Add] add routes

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 const express = require("express");
+const cors = require("cors");
+const stripe = require("./services/stripe");
+
+const handleError = require("./middleware/handleError");
 
 const app = express();
 
-app.arguments(express.json());
+app.use(cors());
+app.use(express.json());
 
-app.get("/payment", (req, res) => {
-  res.send({
-    message: "payment success",
-  });
+app.get("/plans", async (req, res, next) => {
+  try {
+    const response = await stripe.prices.list({
+      limit: 10,
+      expand: ["data.product"],
+    });
+
+    res.send(response.data);
+  } catch (error) {
+    handleError(error, req, res, next);
+  }
 });
 
-app.listen(5000, () => console.log("server 5000 🚀"));
+app.post("/create-checkout-session", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const { priceId, quantity } = req.body;
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      line_items: [
+        {
+          price: priceId,
+          quantity,
+        },
+      ],
+      success_url: "http://localhost:3000",
+      cancel_url: "http://localhost:3000",
+    });
+
+    console.log(session);
+
+    return res.redirect(303, session.url);
+  } catch (error) {
+    handleError(error, req, res, next);
+  }
+});
+
+app.get("/payment", async (req, res, next) => {
+  try {
+    // create customer
+    // const customer = await stripe.customers.create({
+    //   description: "Test create new customer",
+    // });
+
+    // rethreive products plans
+    const subscription = await stripe.subscriptions.retrieve();
+
+    res.send({
+      message: "create success",
+      // customer,
+      subscription,
+    });
+  } catch (error) {
+    handleError(error, req, res, next);
+  }
+});
+
+app.listen(5001, () => console.log("server 5001 🚀"));
