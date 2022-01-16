40462a8577bbb5e2014bc931c78b9a39633a95ad
xeusteerapat <xeus085@gmail.com>
2022-01-16 19:37:01 +0700

    :boom: [Update] update proper error status code

--- middleware/handleError.js
+++ middleware/handleError.js
@@ -1,9 +1,8 @@
 module.exports = function handleError(err, req, res, next) {
   if (err) {
-    res.send({
+    res.status(500).send({
       status: 500,
       message: err.message,
     });
   }
-  // next(err);
 };
